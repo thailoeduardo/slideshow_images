951ca02c2dfff70a358cdd25a544b38f8cd4bdc4
thailoeduardo <thailoeduardo@hotmail.com>
2016-12-14 13:22:50 -0200

    script page

---
+++ script.js
@@ -0,0 +1,44 @@
+
+		var btnProximoImg = document.getElementById('btnProximoImg');
+
+		btnProximoImg.addEventListener('click', proximaImg)
+
+		setInterval(proximaImg,3000);
+
+		function proximaImg() {
+			var myImage = document.querySelector('img');
+			var mySrc = myImage.getAttribute('src');
+
+			if(mySrc == 'img_1.jpg') {
+				myImage.setAttribute ('src','img_2.jpg');
+			}
+
+			else if  (mySrc == 'img_2.jpg'){
+				myImage.setAttribute ('src','img_3.jpg');
+			}
+
+			else {
+				myImage.setAttribute ('src','img_1.jpg');
+			}
+		};
+
+		var btnAnteriorImg = document.getElementById('btnAnteriorImg');
+
+		btnAnteriorImg.addEventListener('click', anteriorImg)
+
+		function anteriorImg() {
+			var myImage = document.querySelector('img');
+			var mySrc = myImage.getAttribute('src');
+
+			if(mySrc == 'img_2.jpg') {
+				myImage.setAttribute ('src','img_1.jpg');
+			}
+
+			else if  (mySrc == 'img_3.jpg'){
+				myImage.setAttribute ('src','img_2.jpg');
+			}
+
+			else {
+				myImage.setAttribute ('src','img_3.jpg');
+			}
+		};
